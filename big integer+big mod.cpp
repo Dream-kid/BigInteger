f0d2af1e60600d05326959abe85d9b579f24da3e
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2017-09-24 07:48:57 +0600

    Create big integer+big mod.cpp

---
+++ big integer+big mod.cpp
@@ -0,0 +1,278 @@
+##include<bits/stdc++.h>
+#include <list>
+
+#include <set>
+#include <map>
+#include <ctime>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <string>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <numeric>
+#include <cassert>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define pi 2*acos(0.0)
+#define ull unsigned long long
+#define all(v) v.begin(),v.end()
+
+#define sii(t) scanf("%d",&t)
+#define sll(t) scanf("%lld",&t)
+#define sll(t) scanf("%lld",&t)
+#define ssll(a,b) scanf("%lld%lld",&a,&b)
+#define ssd(a,b) scanf("%lf%lf",&a,&b)
+#define sssll(a,b,c) scanf("%lld%lld%lld",&a,&b,&c)
+#define sssld(a,b,c) scanf("%lf%lf%lf",&a,&b,&c)
+#define ssssll(a,b,c,d) scanf("%lld%lld%lld%lld",&a,&b,&c,&d)
+#define Case(no) printf("Case %lld: ",++no)
+#define nl puts("")
+#define p(a) printf("%d\n",a)
+#define pll(a) printf("%lld\n",a)
+#define P(a) printf("%d ",a)
+#define PLL(a) printf("%lld ",a)
+
+
+#define ff first
+#define se second
+#define pb push_back
+#define ST(v) sort(all(v))
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define maxall(v) *max_element(all(v))
+#define minall(v) *min_element(all(v))
+#define cover(a,d) memset(a,d,sizeof(a))
+#define popcount(i) __builtin_popcount(i)                       //count one
+#define output freopen("out.txt","w",stdout)
+#define un(v) ST(v), (v).earse(unique(all(v)),v.end())
+#define common(a,b) ST(a), ST(b), a.erase(set_intersection(all(a),all(b),a.begin()),a.end())
+#define uncommon(a,b) ST(a), ST(b), a.erase(set_symmetric_difference(all(a),all(b),a.begin()),a.end())
+#define flop(m,n,q)             for(ll i=m;i<=n;i+=q)
+////============ CONSTANT ===============////
+#define mx  (100010)
+#define inf 1000000000000000000
+#define eps 1e-9
+#define mod 10007
+////=====================================////
+
+using namespace std;
+#define inf 1000000000000000
+typedef  long long ll;
+struct Bigint
+{
+    // representations and structures
+    string a; // to store the digits
+    int sign; // sign = -1 for negative numbers, sign = 1 otherwise
+
+    // constructors
+    Bigint() {} // default constructor
+    Bigint( string b )
+    {
+        (*this) = b;    // constructor for string
+    }
+
+    // some helpful methods
+    int size()   // returns number of digits
+    {
+        return a.size();
+    }
+    Bigint inverseSign()   // changes the sign
+    {
+        sign *= -1;
+        return (*this);
+    }
+    Bigint normalize( int newSign )   // removes leading 0, fixes sign
+    {
+        for( int i = a.size() - 1; i > 0 && a[i] == '0'; i-- )
+            a.erase(a.begin() + i);
+        sign = ( a.size() == 1 && a[0] == '0' ) ? 1 : newSign;
+        return (*this);
+    }
+
+    // assignment operator
+    void operator = ( string b )   // assigns a string to Bigint
+    {
+        a = b[0] == '-' ? b.substr(1) : b;
+        reverse( a.begin(), a.end() );
+        this->normalize( b[0] == '-' ? -1 : 1 );
+    }
+
+    // conditional operators
+    bool operator < ( const Bigint &b ) const   // less than operator
+    {
+        if( sign != b.sign ) return sign < b.sign;
+        if( a.size() != b.a.size() )
+            return sign == 1 ? a.size() < b.a.size() : a.size() > b.a.size();
+        for( int i = a.size() - 1; i >= 0; i-- ) if( a[i] != b.a[i] )
+                return sign == 1 ? a[i] < b.a[i] : a[i] > b.a[i];
+        return false;
+    }
+    bool operator == ( const Bigint &b ) const   // operator for equality
+    {
+        return a == b.a && sign == b.sign;
+    }
+
+
+
+    Bigint operator + ( Bigint b )   // addition operator overloading
+    {
+        if( sign != b.sign ) return (*this) - b.inverseSign();
+        Bigint c;
+        for(int i = 0, carry = 0; i<a.size() || i<b.size() || carry; i++ )
+        {
+            carry+=(i<a.size() ? a[i]-48 : 0)+(i<b.a.size() ? b.a[i]-48 : 0);
+            c.a += (carry % 10 + 48);
+            carry /= 10;
+        }
+        return c.normalize(sign);
+    }
+    Bigint operator - ( Bigint b )   // subtraction operator overloading
+    {
+        if( sign != b.sign ) return (*this) + b.inverseSign();
+        int s = sign;
+        sign = b.sign = 1;
+        if( (*this) < b ) return ((b - (*this)).inverseSign()).normalize(-s);
+        Bigint c;
+        for( int i = 0, borrow = 0; i < a.size(); i++ )
+        {
+            borrow = a[i] - borrow - (i < b.size() ? b.a[i] : 48);
+            c.a += borrow >= 0 ? borrow + 48 : borrow + 58;
+            borrow = borrow >= 0 ? 0 : 1;
+        }
+        return c.normalize(s);
+    }
+    Bigint operator * ( Bigint b )   // multiplication operator overloading
+    {
+        Bigint c("0");
+        for( int i = 0, k = a[i] - 48; i < a.size(); i++, k = a[i] - 48 )
+        {
+            while(k--) c = c + b; // ith digit is k, so, we add k times
+            b.a.insert(b.a.begin(), '0'); // multiplied by 10
+        }
+        return c.normalize(sign * b.sign);
+    }
+    Bigint operator / ( Bigint b )   // division operator overloading
+    {
+        if( b.size() == 1 && b.a[0] == '0' ) b.a[0] /= ( b.a[0] - 48 );
+        Bigint c("0"), d;
+        for( int j = 0; j < a.size(); j++ ) d.a += "0";
+        int dSign = sign * b.sign;
+        b.sign = 1;
+        for( int i = a.size() - 1; i >= 0; i-- )
+        {
+            c.a.insert( c.a.begin(), '0');
+            c = c + a.substr( i, 1 );
+            while( !( c < b ) ) c = c - b, d.a[i]++;
+        }
+        return d.normalize(dSign);
+    }
+    Bigint operator % ( Bigint b )   // modulo operator overloading
+    {
+        if( b.size() == 1 && b.a[0] == '0' ) b.a[0] /= ( b.a[0] - 48 );
+        Bigint c("0");
+        b.sign = 1;
+        for( int i = a.size() - 1; i >= 0; i-- )
+        {
+            c.a.insert( c.a.begin(), '0');
+            c = c + a.substr( i, 1 );
+            while( !( c < b ) ) c = c - b;
+        }
+        return c.normalize(sign);
+    }
+
+    // output method
+    void print()
+    {
+        if( sign == -1 ) putchar('-');
+        for( int i = a.size() - 1; i >= 0; i-- ) putchar(a[i]);
+    }
+};
+
+string big_int (string input,string input1,ll o)
+{
+    Bigint a, b, c; // declared some Bigint variables
+
+
+    a = input; // assign the string to Bigint a
+
+    b = input1; // assign the string to Bigint b
+
+    if(o==1)  c = a + b; // adding a and b
+// printing the Bigint
+
+
+    else if(o==2) c = a - b; // subtracting b from a
+
+
+    else if(o==3) c = a * b; // multiplying a and b
+
+    else if(o==4)c = a / b; // dividing a by b
+
+    else   c = a % b; // a modulo b
+    return c.a;
+}
+
+
+string big_mod(string base, string power)
+{
+    string p12 = big_int(power,"2",5);
+    if(power=="0")    return "1";
+
+    else if(p12=="1") //power বেজোড় হলে
+    {
+        string p1 = big_int(base,"9",5);
+        power=big_int(power,"1",2);
+        string p2 = big_mod(base,power);
+        p2= big_int(p2,"9",5);
+        string p3=big_int(p1,p2,3);
+        string p4=big_int(p3,"9",5);
+        return p4;
+    }
+    else
+    {
+        string p1 = big_int(power,"2",4);
+        string p2 = big_mod(base,p1);
+        p2= big_int(p2,"9",5);
+        string p3=big_int(p2,p2,3);
+        string p4=big_int(p3,"9",5);
+        return p4;
+    }
+}
+int main()
+{
+    ll d,t=0;
+    sll(d);
+    while(d--)
+    {
+
+       string  a,b;
+        cin>>a>>b;
+        Case(t);
+        string z=big_mod(a,b);
+        if(z=="0") z="9";
+       cout<<z<<endl;
+
+    }
+
+}
+
+ big-integer
